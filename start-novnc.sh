7e6d46fd6c0f6f4305668ffb1ad6cc60eefaf51e
nekomimi0128 <Nekomimi.0128@gmail.com>
2025-08-19 19:22:43 +0900

    Update start-novnc.sh

--- start-novnc.sh
+++ start-novnc.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-# 既存のVNCセッションを削除
-sudo -u SHUKUSAI vncserver -kill :1 > /dev/null 2>&1 || true
+# 既存 VNC セッションを削除
+su - SHUKUSAI -c "vncserver -kill :1" > /dev/null 2>&1 || true
 
-# VNCサーバー起動
-sudo -u SHUKUSAI vncserver :1 -geometry 1280x800 -depth 24
+# VNC 起動
+su - SHUKUSAI -c "vncserver :1 -geometry 1280x800 -depth 24"
 
-# noVNC起動（再起動時はポートが開放されるまで少し待つ）
-while ! nc -z localhost 5901; do
-  sleep 1
-done
-websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
+# noVNC 起動
+su - SHUKUSAI -c "websockify --web=/usr/share/novnc/ 6080 localhost:5901 &"
